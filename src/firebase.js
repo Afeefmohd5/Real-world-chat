d3190814c02eaee0deea79942033dc5d07098ce9
afeef mohd <afeefmohd68@gmail.com>
2025-02-20 17:41:03 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
